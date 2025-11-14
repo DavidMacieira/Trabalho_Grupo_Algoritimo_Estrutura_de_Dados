c804135c2c33173af6f31863c57e19859465b31c
a34325-IPCA <a34325@alunos.ipca.pt>
2025-11-14 10:59:26 +0000

    Adiciona funcao menu e um menu interno na funcao gerir alunos

--- Simulador Sistema Ensino.c
+++ Simulador Sistema Ensino.c
@@ -4,21 +4,8 @@
 #include <locale.h>
 #include <stdio.h>
 int n = -1;
-
-void gerirAlunos(){
-	
-}
-
-void gerirNotas(){
-	
-}
-
-void ordenar(){
-	
-}
-int main(){
-	setlocale(LC_ALL,"Portuguese");
-	while(n!=0){
+void menu(){
+		while(n!=0){
 		printf("1- Gestão Alunos\n2- Gestão Notas\n3- Ordernar Alunos\n0- Sair\n");
 		scanf("%i", &n);
 		switch(n){
@@ -31,5 +18,34 @@ int main(){
 			case 0: return;
 		}
 	}
+}
+void gerirAlunos(){
+		while(n!=0){
+		printf("1- Adicionar Aluno\n2- Remover Aluno\n3- Listar Alunos\n4- Voltar ao Menu Principal\n0- Sair\n");
+		scanf("%i", &n);
+		switch(n){
+			case 1: gerirAlunos();
+				break;
+			case 2: gerirNotas();
+				break;
+			case 3: ordenar();
+				break;
+			case 4: menu();
+				break;
+			case 0: return;
+		}
+	}
+}
+
+void gerirNotas(){
+	
+}
+
+void ordenar(){
+	
+}
+ int main(){
+	setlocale(LC_ALL,"Portuguese");
+	menu();
 }
 
