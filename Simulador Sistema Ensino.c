aeaea657bbb234db3227eddf96f2aa56e1dad2cd
Pedro_Consani <a34325@alunos.ipca.pt>
2025-11-23 12:48:52 +0000

    Implementa adicionar, remover e listar alunos

--- Simulador Sistema Ensino.c
+++ Simulador Sistema Ensino.c
@@ -1,44 +1,18 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <locale.h>
 #include <stdio.h>
-int n = -1;
-float media;
-void menu(){
-		while(n!=0){
-		printf("1- Gestão Alunos\n2- Gestão Notas\n3- Ordernar Alunos\n0- Sair\n");
-		scanf("%i", &n);
-		switch(n){
-			case 1: gerirAlunos();
-				break;
-			case 2: gerirNotas();
-				break;
-			case 3: ordenar();
-				break;
-			case 0: return;
-		}
-	}
-}
-void gerirAlunos(){
-		while(n!=0){
-		printf("1- Adicionar Aluno\n2- Remover Aluno\n3- Listar Alunos\n4- Voltar ao Menu Principal\n0- Sair\n");
-		scanf("%i", &n);
-		switch(n){
-			case 1: adicionarAlunos();
-				break;
-			case 2: removerAluno();
-				break;
-			case 3: listarAlunos();
-				break;
-			case 4: menu();
-				break;
-			case 0: return;
-		}
-	}
-}
+/*int n = -1;
+float media;*/
+char alunos[100][100];
+int totalAlunos = 0;
+int c;
+int i;
 
-void gerirNotas(){
+
+
+
+/*void gerirNotas(){
 		while(n!=0){
 		printf("1- Calcular média de aluno\n2- Calcular média da turma\n3- Voltar ao Menu Principal\n0- Sair\n");
 		scanf("%i", &n);
@@ -53,10 +27,6 @@ void gerirNotas(){
 		}
 	}
 }
-	
-	
-}
-
 void ordenar(){
 		while(n!=0){
 		printf("1- Ordernar alunos porMedia\n2- Ordenar por nome de aluno\n3- Ordenar por número de aluno\n4- Voltar ao Menu Principal\n0- Sair\n");
@@ -74,7 +44,74 @@ void ordenar(){
 	}
 	
 }
- int main(){
+*/
+
+void adicionarAluno(){
+	//Consumindo o ´/n´ do buffer de entrada.
+	while ((c = getchar()) != '\n' && c != EOF) {}
+	printf("Digite o nome do aluno: ");
+	fgets(alunos[totalAlunos], 100, stdin);
+	for (i = 0; i < 3; i++){
+		printf("%s", alunos[i]);
+	}
+	totalAlunos++;
+
+}
+void removerAluno(){
+	char nome[100];
+	while ((c = getchar()) != '\n' && c != EOF) {}
+	printf("Digite o nome ou ID do aluno a ser removido: ");
+	fgets(nome, 100, stdin);
+	for (i = 0; i < 100; i++){
+		if (strcmp(nome, alunos[i])== 0){
+			strcpy(alunos[i], alunos[i+1]);
+			totalAlunos--;
+		}
+	}
+}
+
+void listarAlunos(){
+	for (i = 0; i < totalAlunos; i++){
+		printf("%s", alunos[i]);
+	}
+}
+
+void gerirAlunos(){
+	int n = -1;
+	while (n!= 0){
+		printf("1- Adicionar Aluno\n2- Remover Aluno\n3- Listar Alunos\n0- Voltar Menu Pricinpal\n");
+		scanf("%d", &n);
+		switch(n) {
+			case 1: adicionarAluno();
+				break;
+			case 2: removerAluno();
+				break;
+			case 3: listarAlunos();
+				break;
+			case 0: return;
+		}
+	}
+}
+void menu(){
+	int n = -1;
+		while(n!=0){
+		printf("1- Gestão Alunos\n2- Gestão Notas\n3- Ordernar Alunos\n0- Sair\n");
+		scanf("%i", &n);
+		switch(n){
+			case 1: gerirAlunos();
+				break;
+			//case 2: gerirNotas();
+			//	break;
+			//case 3: ordenar();
+			//	break;
+			case 0: return;
+		}
+	}	
+}
+
+
+
+void main(){
 	setlocale(LC_ALL,"Portuguese");
 	menu();
 }
