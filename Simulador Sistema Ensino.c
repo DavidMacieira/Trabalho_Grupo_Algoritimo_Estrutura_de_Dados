ad4244d491245a76381c33d2043b4c0fd6aca16d
Pedro_Consani <a34325@alunos.ipca.pt>
2025-11-29 12:18:12 +0000

    Implementacao procedimentos de gestao alunos

--- Simulador Sistema Ensino.c
+++ Simulador Sistema Ensino.c
@@ -1,86 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <stdio.h>
-/*int n = -1;
-float media;*/
-char alunos[100][100];
+#include <string.h>
+
+int ids[100];
+char alunos[300][300];
 int totalAlunos = 0;
 int c;
 int i;
+int n;
 
 
+void gerirNotas(){
+	// Implementar procedimento de gerir notas
+	printf("");
 
-
-/*void gerirNotas(){
-		while(n!=0){
-		printf("1- Calcular média de aluno\n2- Calcular média da turma\n3- Voltar ao Menu Principal\n0- Sair\n");
-		scanf("%i", &n);
-		switch(n){
-			case 1: mediaAluno();
-				break;
-			case 2: mediaTurma();
-				break;
-			case 3: menuPrincipal();
-				break;
-			case 0: return;
-		}
-	}
 }
 void ordenar(){
-		while(n!=0){
-		printf("1- Ordernar alunos porMedia\n2- Ordenar por nome de aluno\n3- Ordenar por número de aluno\n4- Voltar ao Menu Principal\n0- Sair\n");
-		scanf("%i", &n);
-		switch(n){
-			case 1: ordernarMedia();
-				break;
-			case 2: ordernarNome();
-				break;
-			case 3: ordernarNumero();
-				break;
-			case 4: 
-			case 0: return;
-		}
-	}
+	// Implementar procedimento de ordenar alunos
+	printf("");
+
 	
 }
-*/
+
 
 void adicionarAluno(){
-	//Consumindo o ´/n´ do buffer de entrada.
-	while ((c = getchar()) != '\n' && c != EOF) {}
-	printf("Digite o nome do aluno: ");
-	fgets(alunos[totalAlunos], 100, stdin);
-	for (i = 0; i < 3; i++){
-		printf("%s", alunos[i]);
-	}
-	totalAlunos++;
 
+	printf("Digite o nome do aluno: ");
+	fgets(alunos[totalAlunos], 300, stdin);
+	printf("\n");
+
+	// Remover o caractere de nova linha, se presente
+	alunos[totalAlunos][strcspn(alunos[totalAlunos], "\n")] = '\0';
+	ids[totalAlunos] = totalAlunos;
+	totalAlunos++;
 }
+
 void removerAluno(){
-	char nome[100];
-	while ((c = getchar()) != '\n' && c != EOF) {}
+	char nome[300];
+	int j;
+
 	printf("Digite o nome ou ID do aluno a ser removido: ");
-	fgets(nome, 100, stdin);
-	for (i = 0; i < 100; i++){
+	fgets(nome, 300, stdin);
+	// Remover o caractere de nova linha, se presente
+	nome[strcspn(nome, "\n")] = '\0';
+
+
+
+	for (i = 0; i < totalAlunos; i++){
+		// Compara o nome armazenado na posiÃ§Ã£o i com o nome fornecido.
 		if (strcmp(nome, alunos[i])== 0){
-			strcpy(alunos[i], alunos[i+1]);
+			printf("----Aluno removido com sucesso!----\n");
+			printf("ID Aluno: %d ||, Nome Aluno: %s\n", ids[i], alunos[i]);
+			for (j = i; j < totalAlunos - 1; j++) {
+				strcpy(alunos[j], alunos[j+1]);
+			}
+
 			totalAlunos--;
 		}
 	}
 }
 
+
 void listarAlunos(){
-	for (i = 0; i < totalAlunos; i++){
-		printf("%s", alunos[i]);
+	for (int i = 0; i < totalAlunos; ++i) {
+		printf("ID Aluno: %d ||, Nome Aluno: %s\n", ids[i], alunos[i]);
 	}
 }
 
+
+
 void gerirAlunos(){
-	int n = -1;
+	n = -1;
 	while (n!= 0){
 		printf("1- Adicionar Aluno\n2- Remover Aluno\n3- Listar Alunos\n0- Voltar Menu Pricinpal\n");
 		scanf("%d", &n);
+		getchar();
 		switch(n) {
 			case 1: adicionarAluno();
 				break;
@@ -89,30 +84,34 @@ void gerirAlunos(){
 			case 3: listarAlunos();
 				break;
 			case 0: return;
+			default: printf("OpÃ§Ã£o invÃ¡lida! Tente novamente.\n");
 		}
 	}
 }
-void menu(){
-	int n = -1;
-		while(n!=0){
-		printf("1- Gestão Alunos\n2- Gestão Notas\n3- Ordernar Alunos\n0- Sair\n");
+int  menu(){
+	int z = -1;
+	setlocale(LC_ALL,"Portuguese");
+	while(z!=0){
+		printf("1- GestÃ£o Alunos\n2- GestÃ£o Notas\n3- Ordernar Alunos\n0- Sair\n");
 		scanf("%i", &n);
 		switch(n){
 			case 1: gerirAlunos();
 				break;
-			//case 2: gerirNotas();
-			//	break;
-			//case 3: ordenar();
-			//	break;
-			case 0: return;
+				//case 2: gerirNotas();
+				//	break;
+				//case 3: ordenar();
+				//	break;
+			case 0: return 0;
+			default: printf("OpÃ§Ã£o invÃ¡lida! Tente novamente.\n");
 		}
-	}	
+	}
 }
 
 
 
-void main(){
+int main(){
 	setlocale(LC_ALL,"Portuguese");
 	menu();
+
 }
 
